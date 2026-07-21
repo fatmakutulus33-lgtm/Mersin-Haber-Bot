5eb461b8b7f5836bd94694ad01d24c34be7447e9
cengo33 <hakankurtulus33@yandex.com>
2026-07-21 21:20:12 +0300

    refactor: cleanup obsolete test files, unused scripts, and unused dependencies

--- tests/test_og.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-axios.get('https://news.google.com/rss/articles/CBMiugFBVV95cUxORUx6Y3FoVktyQUNzekRKanZpajhfUm1aSUdsbk8tYWctaTFQSnFHWm9RUld6MHRnNkFieklEalZ6YW1WOWRCRUNuY1ZZVlVMUUpud3VoaGRnOTgyYzFYUEdtdVpMbTBuZ0dMM1NYMFU0OGV2d3phMjZzZmRnSS1iNFpyWnpYUnZVWGNXcFFqSjRHNFgwR19NNk42Q0xNM0tyaTIzZ1JTZmZDNGhhblgyLU1fTlU0OXVkU1E?oc=5')
-.then(r => {
-  const match = r.data.match(/<meta[^>]*property="og:image"[^>]*content="([^"]+)"/i);
-  console.log('og:image:', match ? match[1] : 'not found');
-})
-.catch(e => console.error(e.message));
